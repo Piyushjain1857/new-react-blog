22adabb76de1e8634fe9dcc9fb0d47ef582dd361
Piyush <piyushjain1857@gmail.com>
2026-04-22 11:17:01 +0530

    added css

--- src/components/Bollywood.jsx
+++ src/components/Bollywood.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Data from '../Data.js'
+import '../css/Bollywood.css'
 
 const Bollywood = () => {
     const bollywoodData = Data.filter((item) => item.category === 'Bollywood')
     return (
-        <div>
-            <h1>Bollywood</h1>
+        <div className="bollywood-container">
+            <h1 className="bollywood-title">Bollywood</h1>
             {bollywoodData.map((bollydata) => (
-                <div key={bollydata.id}>
-                    <h2>{bollydata.title}</h2>
-                    <img src={bollydata.img_url} alt={bollydata.title} />
-                    <p>{bollydata.description}</p>
+                <div key={bollydata.id} className="bollywood-card">
+                    <h2 className="bollywood-card-title">{bollydata.title}</h2>
+                    <img className="bollywood-image" src={bollydata.img_url} alt={bollydata.title} />
+                    <p className="bollywood-description">{bollydata.description}</p>
                 </div>
             ))}
         </div>
