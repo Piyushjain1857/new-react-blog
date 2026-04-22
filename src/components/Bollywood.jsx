e978c9694096769df9458ccc34df5294e6e3c48e
Piyush <piyushjain1857@gmail.com>
2026-04-22 11:02:03 +0530

    added data.js

--- src/components/Bollywood.jsx
+++ src/components/Bollywood.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-// import data from '../data.js'
+import Data from '../Data.js'
 
 const Bollywood = () => {
-    // const bollywoodData = data.filter((item) => item.category === 'Bollywood')
+    const bollywoodData = Data.filter((item) => item.category === 'Bollywood')
     return (
         <div>
             <h1>Bollywood</h1>
-            {/* {bollywoodData.map((bollydata) => (
+            {bollywoodData.map((bollydata) => (
                 <div key={bollydata.id}>
                     <h2>{bollydata.title}</h2>
                     <img src={bollydata.img_url} alt={bollydata.title} />
                     <p>{bollydata.description}</p>
                 </div>
-            ))} */}
+            ))}
         </div>
     )
 }
